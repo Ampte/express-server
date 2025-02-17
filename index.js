120124f24ca8ab68e6f990d0e89da264302e30f7
Ampte B Marak <amptebmarak@gmail.com>
2025-02-18 00:39:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`App is running on ${port}`);
+});
+
+const data_file = 'data.json';
+
+// Load existing data
+let formData = [];
+if (fs.existsSync(data_file)) {
+    const fileContent = fs.readFileSync(data_file, 'utf8');
+    formData = fileContent ? JSON.parse(fileContent) : [];
+};
+
+app.post('/add-name', (req, res) => {
+    const newEntry = req.body;
+    formData.push(newEntry);
+
+// Save to json file
+    fs.writeFile(data_file, JSON.stringify(formData, null, 2), (err) => {
+        if(err){
+            res.send("Error");
+        } else{
+            res.send("Data submitted sucessfully");
+        }
+    });
+
+    console.log(newEntry);
+});
+
