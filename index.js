0472be3f09cf3ff990d014fd03affd3b01306887
Ampte B Marak <amptebmarak@gmail.com>
2025-02-19 12:25:08 +0530

    Add few changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(bodyParser.json());
 
